925a855b131f9de1ae3c73651233d6d57abd7add
Bjorn Krols <krols.bj@gmail.com>
2019-08-20 09:21:47 +0200

    Switch to webpack convention to reduce boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,5 @@
-const path = require("path");
-
 module.exports = {
-  entry: path.resolve(__dirname + "/src/userhut.js"),
   output: {
-    path: path.resolve(__dirname + "/dist/"),
-    filename: "userhut.js",
     library: "Userhut",
     libraryTarget: "umd",
     umdNamedDefine: true
